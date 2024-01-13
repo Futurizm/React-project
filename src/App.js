a8ffb85b4a938d036fcd51f1b247683fbf860bee
John <zantrebatyrbek@gmail.com>
2024-01-13 13:28:20 +0600

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar/Navbar.js";
+import Footer from "./components/footer/Footer.js";
+import Home from "./pages/Home.js";
+import Projects from "./pages/Projects.js";
+import Project from "./pages/Project";
+import Contacts from "./pages/Contacts.js";
+
+
+import ScrollToTop from "./utils/scrollToTop.js";
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/project/:id" element={<Project />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/helpers/projectList.js
@@ -0,0 +1,66 @@
+import project01 from "./../img/projects/01.jpg";
+import project01Big from "./../img/projects/01-big.jpg";
+
+import project02 from "./../img/projects/02.jpg";
+import project02Big from "./../img/projects/02-big.jpg";   
+
+import project03 from "./../img/projects/03.jpg";
+import project03Big from "./../img/projects/03-big.jpg";
+
+import project04 from "./../img/projects/04.jpg";
+import project04Big from "./../img/projects/04-big.jpg";
+
+import project05 from "./../img/projects/05.jpg";
+import project05Big from "./../img/projects/05-big.jpg";
+
+import project06 from "./../img/projects/06.jpg";
+import project06Big from "./../img/projects/06-big.jpg";
+
+const projects = [
+    {
+        title: "Gaming streaming portal",
+        skills: 'React, Node.js, MongoDB',
+        imgBig: project01Big,
+        img: project01,
+        gitHubLink: 'https://github.com'
+    }, 
+
+    {
+        title: "Video service",
+        skills: 'React, PHP, MySQL',
+        imgBig: project02Big,
+        img: project02,
+        gitHubLink: 'https://github.com'
+    }, 
+
+    {
+        title: "Video portal",
+        skills: 'Vue JS, Node JS, MongoDB',
+        imgBig: project03Big,
+        img: project03,
+        gitHubLink: 'https://github.com'
+    }, 
+
+    {
+        title: "Dating app",
+        skills: 'React Native',
+        imgBig: project04Big,
+        img: project04,
+    }, 
+
+    {
+        title: "Landing",
+        skills: 'HTML, SCSS, JS',
+        imgBig: project05Big,
+        img: project05,
+    }, 
+
+    {
+        title: "Gaming community",
+        skills: 'React, PHP, MySQL',
+        imgBig: project06Big,
+        img: project06,
+    }, 
+]
+
+export {projects}
